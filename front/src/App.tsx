f18445e19cbe361745c3c64b53bbac5637fa6ab6
Téo <teophile.chardon123@gmail.com>
2024-05-12 17:01:19 +0200

    Add : List all pages

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -5,6 +5,13 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Shop from "./pages/Shop";
 import Categorie from './pages/Categorie';
+import Checkout from './pages/Checkout';
+import Error from './pages/404';
+import Ordersum from './pages/OrderSummary';
+import Product from './components/Products';
+import ShoppingCard from './pages/ShoppingCard';
+
+
 
 function App() {
   return (
@@ -15,6 +22,11 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/categorie" element={<Categorie />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/ordersum" element={<Ordersum />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/shoppingcard" element={<ShoppingCard />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </Router>
   );
